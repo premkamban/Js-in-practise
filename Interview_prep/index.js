2c224b06197ed9e655689d0ba8febf339473b49b
Prem <premkamban@gmail.com>
2022-10-11 19:19:55 +0530

    Added interview questions

--- Interview_prep/index.js
+++ Interview_prep/index.js
@@ -9,11 +9,11 @@ let variables are not added in window object
 
 this.<let-variable> is undefined
 */
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
-var a = 10;
-let b = 11;
+// var a = 10;
+// let b = 11;
 //const c = 12;
 
 /* let variable can't be shadowed
@@ -25,10 +25,44 @@ let a = 0;
 //temporal dead zone - there are in the scope by not declared
 
 //2) block scope
-{
-    var a = 1000;
-    var j = 20;
-    // const c = 30;
-}
-var a = 1200;
-console.log(a);
+// {
+//     var a = 1000;
+//     var j = 20;
+//     // const c = 30;
+// }
+// var a = 1200;
+// console.log(a);
+
+// function one() {
+//     var b = 900;
+//     function two() {
+//         var a = 7
+//         function three() {
+//             console.log(a, b);
+//         }
+//         three();
+//     }
+//     two();
+// }
+// one();
+
+// function x() {
+//     for (var i = 1; i <= 5; i++) {
+//         function outerCloure(i) {
+//             setTimeout(() => { console.log(i) }, i * 1000);
+//         }
+//         outerCloure(i);
+//     }
+//     console.log("Timer started:"); 
+// }
+
+// x();
+
+//Funtion statement aka Fun declaration
+
+let count = 0;
+document.getElementById("hello").addEventListener('click', () => {
+    document.getElementById('counter').innerHTML = ++count;
+});
+
+/* HOC */
